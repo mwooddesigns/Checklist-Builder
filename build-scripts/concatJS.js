f8a5b4366bb9e8e1e89bf25e85a1b6884c27dabd
Michael Wood <mwood@markettraders.com>
2016-12-08 08:25:50 -0500

    More build script stuff

---
+++ build-scripts/concatJS.js
@@ -0,0 +1,31 @@
+// var concat = require('concat-files');
+var buildify = require('buildify');
+
+var env = "development";
+
+var prodDependencies = ['./dependencies/vue/dist/vue.min.js'];
+var devDependencies = ['./dependencies/vue/dist/vue.js'];
+var srcFiles = ['./src/js/index.js'];
+
+switch (env) {
+  case "development":
+    console.log("Running development concatenation.");
+    var files = devDependencies.concat(srcFiles);
+
+    buildify()
+      .concat(files)
+      .save("./dist/js/master.js");
+    console.log("master.js file created");
+    break;
+  case "production":
+    console.log("Running production concatenation.");
+    var files = prodDependencies.concat(srcFiles);
+    buildify()
+      .concat(files)
+      .save("./dist/js/master.js");
+    console.log("master.js file created");
+    break;
+  default:
+    console.log("An error occured: Unknown Environment");
+    break;
+}
