c92f2d68858f868df3167cbe3c5fe9b41bca44f6
Michael Wood <mwood@markettraders.com>
2016-12-09 10:12:27 -0500

    Changing auto increment value

--- DB_STRUCT.sql
+++ DB_STRUCT.sql
@@ -27,7 +27,7 @@ CREATE TABLE `checklists` (
   `name` varchar(45) DEFAULT NULL,
   `config` varchar(8000) DEFAULT NULL,
   PRIMARY KEY (`checklist_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
